ef1f5c65b68bd8a07bc9b7e5b4e7dc8124bad540
zsl <1300326388@qq.com>
2015-05-09 20:00:51 +0800

    高斯模糊完毕

---
+++ app/src/main/java/com/zsl/fglass/MainActivity.java
@@ -0,0 +1,64 @@
+package com.zsl.fglass;
+
+import android.annotation.SuppressLint;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import static com.zsl.fglass.utils.FastBlur.blur;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView image;
+    private TextView text;
+    private TextView statusText;
+    private CheckBox cb_fastBlur;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+
+
+    }
+
+    private void initData() {
+
+    }
+
+    private void initView() {
+        image = (ImageView) findViewById(R.id.picture);
+        text = (TextView) findViewById(R.id.text);
+        cb_fastBlur = (CheckBox) findViewById(R.id.main_cb_fastblur);
+        statusText = addStatusText((ViewGroup) findViewById(R.id.controls));
+        cb_fastBlur.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    blur(image, text, 3, 8, statusText);
+                }else{
+                    text.setBackgroundColor(Color.parseColor("#00ffffff"));
+                }
+            }
+        });
+    }
+
+    private TextView addStatusText(ViewGroup container) {
+        TextView result = new TextView(this);
+        result.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        result.setTextColor(0xFFFFFFFF);
+        container.addView(result);
+        return result;
+    }
+
+}
